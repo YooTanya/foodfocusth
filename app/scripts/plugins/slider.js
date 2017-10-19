b31028b8b69901f3073ac7cdb7226fcbecb0622e
TanyaR <tanya.r@aleph-labs.com>
2017-10-19 21:22:07 +0700

    autoplay carousel, removed submenu

--- app/scripts/plugins/slider.js
+++ app/scripts/plugins/slider.js
@@ -38,66 +38,30 @@
 
   $(function() {
     $('.banner-carousel').slick({
-       infinite: true,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       dots: true
-    });
-    $('.banner-v-carousel').slick({
+      autoplay: true,
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
-      arrows : false,
-      dots: true,
-      vertical: true,
-      verticalSwiping: true
+      dots: true
     });
+    
     $('.carousel').each(function() {
       if($(this).find('.item').length > 3) {
         $(this).slick({
-           mobileFirst: true,
-           variableWidth: true,
-           responsive: [{
+          autoplay: true,
+          mobileFirst: true,
+          variableWidth: true,
+          responsive: [{
               breakpoint: 768,
               settings: {
-                 infinite: true,
-                 slidesToShow: 3,
-                 centerMode: true,
-                 dots: false
+                infinite: true,
+                slidesToShow: 3,
+                centerMode: true,
+                dots: false
               }
-            }]
+          }]
         });
       }
-      else {
-        $(this).addClass('static-carousel');
-      }
-    });
-    var slideNumber = Math.floor($('.inner-container-left').width()/$('.carousel-left .item').width());
-    $('.carousel-left').slick({
-       infinite: false,
-       dots: false,
-       slidesToShow: slideNumber,
-       variableWidth: true
-    });
-
-    $('.main-img').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      fade: true,
-      infinite: true,
-      asNavFor: '.tiny-img'
-    });
-    var slideTinyNumber = Math.floor($('.tiny-img').width()/$('.tiny-img img').width());
-    $('.tiny-img').slick({
-      slidesToShow: slideTinyNumber,
-      slidesToScroll: 1,
-      asNavFor: '.main-img',
-      dots: false,
-      arrows: false,
-      variableWidth: true,
-      focusOnSelect: true,
-      infinite: true
     });
   });
 }(jQuery, window));
